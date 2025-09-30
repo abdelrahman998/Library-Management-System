59aa87d6abdcc120d8826000ab6457fd4b1fa289
abdelrahman <a.alesawy98@gmail.com>
2025-09-30 10:56:22 +0300

    remove unused methods from BookRepository

--- src/main/java/com/library/repository/BookRepository.java
+++ src/main/java/com/library/repository/BookRepository.java
@@ -14,21 +14,14 @@ import java.util.Optional;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    // Find by ISBN
-    Optional<Book> findByIsbn(String isbn);
-
     // Check if book exists by ISBN
     boolean existsByIsbn(String isbn);
 
     // Search by title (case-insensitive)
     Page<Book> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 
-
     // Find available books (availableCopies > 0)
     @Query("SELECT b FROM Book b WHERE b.availableCopies > 0")
     Page<Book> findAvailableBooks(Pageable pageable);
-
-    // Find by language
-    List<Book> findByLanguage(String language);
 
 }
